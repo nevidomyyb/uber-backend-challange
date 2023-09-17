4e486595f6c7bfa543d656bbae1967c31dd70ba7
Pedro Cunha <idk.pedroc@gmail.com>
2023-09-17 20:23:24 -0300

    create flask app

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from flask_smorest import Api
+
+def create_app():
+    app = Flask(__name__, instance_path=os.getcwd())
+
+    app.config["PROPAGATION_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores Rest API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+
+    api = Api(app)
+
+    return app
