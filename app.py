6df90f7530048cc705f8dc835e37354192a64769
Pedro Cunha <idk.pedroc@gmail.com>
2023-09-17 22:04:51 -0300

    add: email_sender and registering blueprint in app

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask
 from flask_smorest import Api
 
+from resources.email_sender import blp as EmailSenderBlueprint
+
 def create_app():
     app = Flask(__name__, instance_path=os.getcwd())
 
@@ -14,5 +16,8 @@ def create_app():
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
     api = Api(app)
+
+    api.register_blueprint(EmailSenderBlueprint)
+
 
     return app
---
+++ resources/email_sender.py
@@ -0,0 +1,24 @@
+from flask.views import MethodView
+from flask import jsonify
+from flask_smorest import Blueprint, abort
+from services.aws_ses_service.ses_service import AWSSESEmailService
+from schemas.email_schema import EmailSchema
+
+blp = Blueprint("email", __name__, description='Email sender')
+
+@blp.route('/api/send_mail/aws/')
+class EmailSender(MethodView):
+    @blp.arguments(EmailSchema)
+    @blp.response(200, EmailSchema)
+    def post(self, email_data):
+        EmailService = AWSSESEmailService(region_name='sa-east-1')
+        response =EmailService.send_email(
+            to=email_data["to"],
+            subject=email_data["subject"],
+            body=email_data["subject"]
+        )
+        print(response)
+        if response["code"] == 200:
+            return jsonify({"code": 200, "message": "Email sent"})
+        else:
+            abort(500, message="An error occured while sending email")
