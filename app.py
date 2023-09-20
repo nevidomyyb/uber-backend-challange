34b06803bdacf46a7b67e82bab519264b5d5b79c
Pedro Cunha <idk.pedroc@gmail.com>
2023-09-19 21:55:52 -0300

    add: loadenv in create app function and checkservice resource

--- app.py
+++ app.py
@@ -5,11 +5,20 @@ from flask_smorest import Api
 from resources.aws_email_sender import blp as EmailSenderAWSBlueprint
 from resources.router_email_sender import blp as EmailRouterBlueprint
 from resources.flask_email_sender import blp as EmailSenderFlaskBlueprint
+from resources.check_services import blp as CheckServicesBlueprint
+
+from dotenv import load_dotenv
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 from log import createArchive, checkArchiveExistence
 
 def create_app():
     app = Flask(__name__, instance_path=os.getcwd())
+
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
+    load_dotenv(os.path.join(basedir, '.flaskenv'))
 
     app.config["PROPAGATION_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Email Service Api"
@@ -23,9 +32,14 @@ def create_app():
     api.register_blueprint(EmailSenderAWSBlueprint)
     api.register_blueprint(EmailSenderFlaskBlueprint)
     api.register_blueprint(EmailRouterBlueprint)
+    api.register_blueprint(CheckServicesBlueprint)
 
     exist = checkArchiveExistence("log.txt")
     if not exist:
         createArchive('log.txt')
 
     return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
